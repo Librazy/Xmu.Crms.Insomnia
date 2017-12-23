1883e96f90395d7a4db1ad796c746f1275bd7ec3
Librazy <im@librazy.org>
2017-12-24 03:08:40 +0800

    use tests

--- Xmu.Crms.Insomnia/Program.cs
+++ Xmu.Crms.Insomnia/Program.cs
@@ -34,67 +34,6 @@ namespace Xmu.Crms.Insomnia
                 {
                     await db.Database.MigrateAsync();
                 }
-
-                if (Convert.ToBoolean(conf["Database:InsertStub"]))
-                {
-                    var school = await db.School.AddAsync(new School
-                    {
-                        City = "厦门",
-                        Name = "厦门市人民公园",
-                        Province = "福建"
-                    });
-
-                    await db.SaveChangesAsync();
-
-                    await db.UserInfo.AddAsync(new UserInfo
-                    {
-                        Avatar = "/upload/avatar/Logo_Li.png",
-                        Email = "t@t.test",
-                        Gender = Gender.Male,
-                        Name = "张三",
-                        Number = "123456",
-                        Password = PasswordUtils.HashString("123"),
-                        Phone = "1234",
-                        School = await db.School.FindAsync(school.Entity.Id),
-                        Title = Title.Other
-                    });
-
-                    await db.UserInfo.AddAsync(new UserInfo
-                    {
-                        Avatar = "/upload/avatar/Logo_Li.png",
-                        Email = "t2@t.test",
-                        Gender = Gender.Female,
-                        Name = "李四",
-                        Number = "134254",
-                        Password = PasswordUtils.HashString("456"),
-                        Phone = "123",
-                        School = await db.School.FindAsync(school.Entity.Id),
-                        Title = Title.Professer
-                    });
-
-                    await db.SaveChangesAsync();
-                }
-
-                if (Convert.ToBoolean(conf["Database:Check"]))
-                {
-                    var checkSetting = new JsonSerializerSettings();
-                    checkSetting.Converters.Add(new StringEnumConverter());
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.Attendences.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.ClassInfo.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.Course.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.CourseSelection.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.FixGroup.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.FixGroupMember.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.Location.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.Seminar.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.SeminarGroup.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.SeminarGroupMember.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.SeminarGroupTopic.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.StudentScoreGroup.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.Topic.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.UserInfo.SingleOrDefaultAsync(a => a.Id == 1), checkSetting));
-                    Debug.WriteLine(JsonConvert.SerializeObject(await db.UserInfo.SingleOrDefaultAsync(a => a.Id == 3), checkSetting));
-                }
             }
 
             host.Run();
